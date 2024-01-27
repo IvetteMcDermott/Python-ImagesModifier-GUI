d4b060a78321161013f64e970c4a8c1994df111c
IvetteMcDermott <ivettedemcdermott@gmail.com>
2024-01-27 20:50:20 +0000

    Modify to improve the new upload
    Manage if it didnt select one image. And improve gui

--- remover-bg-w-upload-btn.py
+++ remover-bg-w-upload-btn.py
@@ -16,12 +16,14 @@ def forget(widget):
     widget.pack_forget() 
 
 # Starter variables
-old_image = "a.png"
 output_image = "bgRemoved.png"
 
 
 # Upload Image Function
 def upload_img():
+    global old_image
+    old_image = "a.png"
+
     # To open dialog box to upload the image 
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
     if file_path:
@@ -30,14 +32,17 @@ def upload_img():
         # save the image from the path
         orig_img.save(old_image)
 
-    # Hide the upload btn
-    forget(upload_btn)
-
-    global textUpl
-    textUpl = Label(root, text='Image uploaded')
-    # Display elements
-    textUpl.pack()
-    remove_btn.pack()
+        # Hide the upload btn
+        forget(upload_btn)
+        forget(welcome)
+        global textUpl
+        textUpl = Label(root, text='Status: Image uploaded', fg='#013237', font=("Helvetica"))
+        # Display elements
+        textUpl.pack()
+        remove_btn.pack()
+    else:
+        # manage in case no picture is upload. brings the first button
+        upload_btn.pack()
 
 # function to remove the bg with library
 def remove_background(old_image):
@@ -48,7 +53,7 @@ def remove_background(old_image):
     global textBgR
     # remove 
     new_img = remove(orig_img)
-    textBgR = Label(root, text='Bg removed')
+    textBgR = Label(root, text='Status: Background removed', fg='#013237', font=("Helvetica"))
     textBgR.pack()
     # call the function to open the result img
     open_image(new_img)
@@ -62,7 +67,7 @@ def save_img(new_img):
     if file_path:
         new_img.save(file_path)
         forget(save_btn)
-        text = Label(root, text="saved")
+        text = Label(root, text="Status: Image Saved", fg='#013237', font=("Helvetica"))
         text.pack()
         forget(textBgR)
         forget(frame)
@@ -101,19 +106,27 @@ def upload_next_img():
 root = Tk()
 
 # window setting
-root.title('Image Bg Remover')
+root.title('Image Background Remover')
 root.geometry("550x550")
+# this would give transparency to the root
+# root.attributes('-alpha',0.8)
+root.configure(bg='#eaf9e7')
+
 # element to give space
-e = Label(root, width=15, borderwidth=2, font=1.05, justify='right')
+e = Label(root, width=15,font=1.05, bg='#eaf9e7')
 e.pack()
 
 # btns
-upload_btn = Button(root, text="Upload Image", padx=18, pady=4, bg='#5790ab', bd=1, fg='#d0d7e1', activebackground='#064469', font=("Helvetica"), command=upload_img)
-remove_btn = Button(root, text="Remove Bg", padx=18, pady=4, bg='#5790ab', bd=1, fg='#d0d7e1', activebackground='#064469', font=("Helvetica"), command=lambda: remove_background(old_image))
-save_btn = Button(root, text="Save Image", padx=18, pady=4, bg='#5790ab', bd=1, fg='#d0d7e1', activebackground='#064469', font=("Helvetica"), command=lambda:save_img(new_img))
-restart_btn = Button(root, text="Upload New Image", padx=18, pady=4, bg='#5790ab', bd=1, fg='#d0d7e1', activebackground='#064469', font=("Helvetica"), command=upload_next_img)
+upload_btn = Button(root, text="Upload Image", padx=18, pady=8, bg='#4ca771', bd=1, fg='#013237', activebackground='#2e6645', border=1, font=("Helvetica"), command=upload_img)
+remove_btn = Button(root, text="Remove Bg", padx=18, pady=4, bg='#4ca771', bd=1, fg='#013237', activebackground='#2e6645', font=("Helvetica"), command=lambda: remove_background(old_image))
+save_btn = Button(root, text="Save Image", padx=18, pady=4, bg='#4ca771', bd=1, fg='#013237', activebackground='#2e6645', font=("Helvetica"), command=lambda:save_img(new_img))
+restart_btn = Button(root, text="Upload New Image", padx=18, pady=4, bg='#4ca771', bd=1, fg='#013237', activebackground='#2e6645', font=("Helvetica"), command=upload_next_img)
 
-# call the firts btn, to upluad
+welcome = Label(root, text='Welcome! For start please upload an image', fg='#013237', font=("Helvetica"))
+
+
+# call message and the first btn, to upload
+welcome.pack()
 upload_btn.pack()
 
 # calls the TK window 
